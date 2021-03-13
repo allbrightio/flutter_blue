cb5f8ebd0ba2ce888a8e6a18bebce681083b585f
Marek Gocał <marek@upnext.io>
2021-03-13 23:05:28 +0100

    add windows compativility layer

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_blue/flutter_blue_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterBluePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterBluePlugin"));
+}
